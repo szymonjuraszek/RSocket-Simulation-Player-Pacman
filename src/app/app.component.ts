e75c23eb979f839a559d08f6546b3a86b6f984b7
Szymon <szymonjuraszek777@gmail.com>
2020-10-15 15:36:20 +0200

    Add two additional players

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import {DownloadService} from './downloader/DownloadService';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  private simulationConnection = new Array(5);
+  private simulationConnection = new Array(7);
   private downloadService: DownloadService;
   private readonly measurementService: MeasurementService;
 
@@ -21,7 +21,7 @@ export class AppComponent {
     this.measurementService = new MeasurementService();
     this.downloadService = new DownloadService(this.measurementService);
 
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 7; i++) {
       this.simulationConnection[i] = new RSocketSimulationConnection(examplePlayers[i].nickname, this.measurementService);
       this.simulationConnection[i].initializeConnection(examplePlayers[i], 1000 + 10000 * i);
     }
